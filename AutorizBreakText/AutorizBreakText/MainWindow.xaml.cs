71e1918b1895c415de695f70288d6a4035dbcf1f
Dion252 <81914287+Dion252@users.noreply.github.com>
2021-04-05 23:34:34 +0300

    Add files via upload

---
+++ AutorizBreakText/AutorizBreakText/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using AutorizBreakText.Winds;
+
+namespace AutorizBreakText
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        string path1 = @"C:\Users\WSR\source\repos\AutorizBreakText\Rez1.txt";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void ClPerson()
+        { 
+            
+        }
+
+
+        private void FirstName_PreviewTextInput_1(object sender, TextCompositionEventArgs e)
+        {
+            int fn;
+            if (int.TryParse(e.Text, out fn))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void LastName_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            int ln;
+            if (int.TryParse(e.Text, out ln))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void AgeName_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            int ag;
+            if (!int.TryParse(e.Text, out ag))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void AutorizBtn1_Click(object sender, RoutedEventArgs e)
+        {
+            string text = LastName.Text + "#" + FirstName.Text + "#" + AgeName.Text;
+
+            using (StreamWriter sw = new StreamWriter(path1))
+            {
+                sw.WriteLine(text);
+            }
+        }
+
+        private void UserList1_Click(object sender, RoutedEventArgs e)
+        {
+           
+            WList1 NewList = new WList1();
+            //this.Hide();
+            NewList.ShowDialog();
+            this.Show();
+
+        }
+    }
+}
---
+++ AutorizBreakText/AutorizBreakText/Winds/WList1.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace AutorizBreakText.Winds
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class WList1 : Window
+    {
+        List<Person> personList = new List<Person>();
+
+
+        string path1 = @"C:\Users\WSR\source\repos\AutorizBreakText\Rez1.txt";
+
+        public WList1()
+        {
+
+            InitializeComponent();
+            StreamReader sw = new StreamReader(path1);
+            string Text = sw.ReadLine();
+            string[] User = Text.Split('#');
+            personList.Add(new Person {Lname = User[0],
+                                       Fname = User[1],
+                                         Age = Int32.Parse(User[2])
+            });
+
+
+            DtGrid1.ItemsSource = personList;
+
+
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+          
+            this.Close();
+
+        }
+    }
+}
